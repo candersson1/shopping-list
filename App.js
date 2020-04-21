b3b31f61c0212af1acb20293a8fc1a7bfa04dc4f
Chris Andersson <candersson@klarsolutions.com>
2020-04-21 08:03:26 -0400

    working now

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {
   ActivityIndicator,
   FlatList,
@@ -10,13 +10,42 @@ import {
   TouchableOpacity,
   Button,
 } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useLinking} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import AddNew from './components/add_new';
+import {useFocusEffect} from '@react-navigation/native';
+
 export default App = () => {
+  const [isReady, setIsReady] = React.useState(false);
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const Stack = createStackNavigator();
+  const ref = React.useRef();
+  const {getInitialState} = useLinking(ref, {
+    prefixes: ['shoppinglist://'],
+  });
+  const [initialState, setInitialState] = React.useState();
+
+  useEffect(() => {
+    Promise.race([
+      getInitialState(),
+      new Promise((resolve) =>
+        // Timeout in 150ms if `getInitialState` doesn't resolve
+        // Workaround for https://github.com/facebook/react-native/issues/25675
+        setTimeout(resolve, 150),
+      ),
+    ])
+      .catch((e) => {
+        console.error(e);
+      })
+      .then((state) => {
+        if (state !== undefined) {
+          setInitialState(state);
+        }
+
+        setIsReady(true);
+      });
+  }, [getInitialState]);
 
   useEffect(getListFromApi, []);
 
@@ -57,6 +86,14 @@ export default App = () => {
   }
 
   function HomeScreen({navigation}) {
+    React.useEffect(() => {
+      const unsubscribe = navigation.addListener('focus', () => {
+        getListFromApi();
+      });
+
+      return unsubscribe;
+    }, [navigation]);
+
     return (
       <SafeAreaView style={styles.container}>
         <FlatList
@@ -72,9 +109,8 @@ export default App = () => {
       </SafeAreaView>
     );
   }
-
   return (
-    <NavigationContainer>
+    <NavigationContainer initialState={initialState} ref={ref}>
       <Stack.Navigator>
         <Stack.Screen name="List" component={HomeScreen} />
         <Stack.Screen name="Add New" component={AddNew} />
--- components/add_new.js
+++ components/add_new.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component, useState} from 'react';
+import {Text, TextInput, View, Button} from 'react-native';
 
-function AddNew() {
+export default function AddNew({navigation}) {
+  const [text, setText] = useState('');
+
   return (
-    <View>
-      <Text> Testing Add New </Text>
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="What do we need?"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
+      <Button
+        onPress={submitToApi.bind(this, text)}
+        title="Add"
+        color="#841584"
+      />
     </View>
   );
-}
 
-export default AddNew;
+  function submitToApi(text) {
+    return fetch(`https://fierce-refuge-84728.herokuapp.com/items`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: 'active',
+        name: text,
+      }),
+    })
+      .then((response) => response.json())
+      .then(navigation.navigate('List'))
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
